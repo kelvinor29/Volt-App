0322c9adf9569b727aa77df9b630c0e03ef7fff8
Kelvinor29 <kelvinor29@gmail.com>
2026-04-03 16:02:40 -0600

    refactor(data): remove routine day full relation

--- app/src/main/java/com/voltfitness/app/data/local/relations/RoutineExerciseWithSets.kt
+++ app/src/main/java/com/voltfitness/app/data/local/relations/RoutineExerciseWithSets.kt
@@ -2,27 +2,17 @@ package com.voltfitness.app.data.local.relations
 
 import androidx.room.Embedded
 import androidx.room.Relation
-import com.voltfitness.app.data.local.entities.RoutineDayEntity
 import com.voltfitness.app.data.local.entities.RoutineExerciseEntity
 import com.voltfitness.app.data.local.entities.RoutineExerciseSetEntity
 
+/**
+ * Room relation representing one routine exercise with all its planned sets.
+ */
 data class RoutineExerciseWithSets(
-    @Embedded
-    val exercise: RoutineExerciseEntity,
+    @Embedded val exercise: RoutineExerciseEntity,
     @Relation(
         parentColumn = "routineExerciseId",
         entityColumn = "routineExerciseId"
     )
     val sets: List<RoutineExerciseSetEntity>
-)
-
-data class RoutineDayFull(
-    @Embedded
-    val day: RoutineDayEntity,
-    @Relation(
-        entity = RoutineExerciseEntity::class,
-        parentColumn = "dayId",
-        entityColumn = "dayId"
-    )
-    val exercisesWithSets: List<RoutineExerciseWithSets>
 )
